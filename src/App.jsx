768ae401097abb9c14cf11d197d23c34c6cff82b
marcushugo <phantomboss300@yahoo.com>
2023-07-08 14:36:45 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      {/* Sidebar start */}
+      My Documents New Document
+      {/* Sidebar end */}
+      {/* Header start */}
+      Document Name Save Changes
+      {/* Header end */}
+      {/* Main start */}
+      Markdown Preview
+      {/* Main end */}
+    </>
+  )
+}
+
+export default App
