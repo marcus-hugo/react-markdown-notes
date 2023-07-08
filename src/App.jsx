b83aba2f985f2e194b75f2eb0fcc92ea7090533d
marcushugo <phantomboss300@yahoo.com>
2023-07-08 15:22:48 -0500

    html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,44 @@ function App() {
   return (
     <>
       {/* Sidebar start */}
-      My Documents New Document
       {/* Sidebar end */}
       {/* Header start */}
-      Document Name Save Changes
+      <header>
+        <div>
+          <img src="src/assets/icon-menu.svg" alt="" />
+        </div>
+        <img src="src/assets/icon-document.svg" alt="" />
+        <div>Document Name</div>
+        <h1>Welcome.md</h1>
+        <img src="src/assets/icon-delete.svg" alt="" />
+        <button>
+          <img src="src/assets/icon-save.svg" alt="" />
+          Save Changes
+        </button>
+        <div>Save Icon</div>
+      </header>
       {/* Header end */}
       {/* Main start */}
+      <main>
+        <div>
+          <div>
+            <h2>MARKDOWN</h2>
+            <textarea value="# Welcome to Markdown" name="" id="" cols="30" rows="10"></textarea>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2>PREVIEW</h2>
+            <img src="src/assets/icon-show-preview.svg" alt="" />
+          </div>
+          <div>
+            <h1>Welcome to Markdown</h1>
+          </div>
+        </div>
+      </main>
       Markdown Preview
       {/* Main end */}
+      <footer>2023 Marcus Hugo. Made with love.</footer>
     </>
   )
 }
