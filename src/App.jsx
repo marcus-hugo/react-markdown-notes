41aad196bcfaa5037ec3c65c71df371b52c79bf6
marcushugo <phantomboss300@yahoo.com>
2023-07-19 08:09:24 -0500

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
   const currentNote = notes.find(note => note.id === currentNoteId) || notes[0]
   const [isOpen, setIsOpen] = useState(false)
 
-  const handleToggle = () => {
+  const handleToggle = e => {
+    e.preventDefault()
     setIsOpen(!isOpen)
   }
 
@@ -171,7 +172,9 @@ function Header({ title, setTitle, deleteCurrentNote, isOpen, handleToggle }) {
 
       <div className="container">
         <button className="header__delete-btn" onClick={deleteCurrentNote}>
-          <img className="icon-delete" src="src/assets/icon-delete.svg" alt="" />
+          <svg width="18" height="20" xmlns="http://www.w3.org/2000/svg">
+            <path className="trashcan" d="M7 16a1 1 0 0 0 1-1V9a1 1 0 1 0-2 0v6a1 1 0 0 0 1 1ZM17 4h-4V3a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v1H1a1 1 0 1 0 0 2h1v11a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V6h1a1 1 0 0 0 0-2ZM7 3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1H7V3Zm7 14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6h10v11Zm-3-1a1 1 0 0 0 1-1V9a1 1 0 0 0-2 0v6a1 1 0 0 0 1 1Z" fill="#7C8187" />
+          </svg>
         </button>
 
         <button type="submit" className="header__button-save">
