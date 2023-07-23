5bcbe304dfede9bdf740d3d8484be2dc8b6fa9be
marcushugo <phantomboss300@yahoo.com>
2023-07-23 15:14:48 -0500

    functions for show/hide modal & editor

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,28 @@ import { nanoid } from "nanoid"
 function App() {
   const [title, setTitle] = useState("")
   const [markdown, setMarkdown] = useState("")
-  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || [{ id: nanoid(), createdAt: new Date().toLocaleDateString(), title: "Demo Note", content: "type note here..." }])
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || [{ id: nanoid(), createdAt: new Date().toLocaleDateString(), title: "untitled-document", content: "type note here..." }])
   const [currentNoteId, setCurrentNoteId] = useState(notes[0]?.id || "")
   const currentNote = notes.find(note => note.id === currentNoteId) || notes[0]
   const [isOpen, setIsOpen] = useState(false)
-  const [show, setShow] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [showEditor, setShowEditor] = useState(true)
 
-  const handleToggle = e => {
+  // Show or hide sidebar
+  function handleToggle(e) {
     e.preventDefault()
     setIsOpen(!isOpen)
+  }
+
+  // Show modal and confirm delete
+  function handleShowModal() {
+    setShowModal(!showModal)
+  }
+
+  // Hide Editor for Previw Mode
+  function handlePreviewMode(e) {
+    e.preventDefault()
+    setShowEditor(!showEditor)
   }
 
   // When notes array is updated - set local storage to notes array
@@ -44,11 +57,6 @@ function App() {
     let allSavedNotes = savedNotes.concat(newlySavedNotes)
     setNotes(allSavedNotes)
     console.log("note updated and saved")
-  }
-
-  // Show modal and confirm delete
-  function showModal() {
-    setShow(!show)
   }
 
   // Delete current note
@@ -83,30 +91,49 @@ function App() {
     })
   }
 
+  // Main Return
   return (
     <div className="app-container">
       <main>
         <Sidebar markdown={markdown} setMarkDown={setMarkdown} notes={notes} setNotes={setNotes} newNote={newNote} saveNote={saveNote} updateNote={updateNote} isOpen={isOpen} />
-        {show && <Modal showModal={showModal} deleteCurrentNote={deleteCurrentNote} title={title} />}
+        {showModal && <Modal handleShowModal={handleShowModal} deleteCurrentNote={deleteCurrentNote} title={title} />}
 
         <form action="" onSubmit={saveNote}>
-          <Header markdown={markdown} notes={notes} setNotes={setNotes} title={title} setTitle={setTitle} deleteCurrentNote={deleteCurrentNote} isOpen={isOpen} setIsOpen={setIsOpen} handleToggle={handleToggle} showModal={showModal} />
+          <Header markdown={markdown} notes={notes} setNotes={setNotes} title={title} setTitle={setTitle} deleteCurrentNote={deleteCurrentNote} isOpen={isOpen} setIsOpen={setIsOpen} handleToggle={handleToggle} handleShowModal={handleShowModal} />
           <div className="editor-container">
             {/* Editor */}
-            <div>
-              <div className="editor__title-container">
-                <h3 className="editor__title">MARKDOWN</h3>
+            {showEditor && (
+              <div>
+                <div className="editor__title-container">
+                  <h3 className="editor__title">MARKDOWN</h3>
+                </div>
+                <textarea placeholder={markdown} value={markdown} onChange={e => setMarkdown(e.target.value)} name="" id="markdown" cols="50" rows="30"></textarea>
               </div>
-              <textarea placeholder={markdown} value={markdown} onChange={e => setMarkdown(e.target.value)} name="" id="markdown" cols="50" rows="30"></textarea>
-            </div>
+            )}
+
             <div className="split"></div>
             {/* Preview */}
             <div className="preview-container">
               <div className="preview__title-container">
                 <h3 className="preview__title">PREVIEW</h3>
-                <img src="src/assets/icon-show-preview.svg" alt="" />
+
+                <button onClick={handlePreviewMode} type="button" className="preview__button">
+                  {showEditor ? (
+                    <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                      <path className="show-icon" d="M7.784.003c4.782-.144 7.597 4.31 8.109 5.198a.8.8 0 0 1 0 .8c-.688 1.2-3.255 5.086-7.677 5.198h-.2c-4.71 0-7.405-4.326-7.909-5.198a.8.8 0 0 1 0-.8C.803 4.001 3.362.115 7.784.003Zm.38 1.6h-.3c-3.199.08-5.286 2.71-6.086 3.998C2.482 6.73 4.73 9.68 8.176 9.6c3.199-.08 5.262-2.711 6.086-3.999-.712-1.127-2.967-4.086-6.398-3.998ZM8 2.803A2.799 2.799 0 1 1 8 8.4a2.799 2.799 0 0 1 0-5.598Zm0 1.599A1.2 1.2 0 1 0 8 6.8a1.2 1.2 0 0 0 0-2.4Z" fill="#7C8187" />
+                    </svg>
+                  ) : (
+                    <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+                      <path className="show-icon" d="M1.38.027a.795.795 0 0 1 .769.206L14.815 12.9a.792.792 0 0 1 0 1.124.792.792 0 0 1-1.124 0L9.234 9.567a2.77 2.77 0 0 1-3.753-3.753L1.024 1.357a.795.795 0 0 1 .357-1.33Zm.998 3.832 1.156 1.116a10.846 10.846 0 0 0-1.773 2.153c.696 1.117 2.929 4.038 6.333 3.959a6.127 6.127 0 0 0 1.346-.198l1.25 1.25a7.505 7.505 0 0 1-2.556.53h-.198c-4.663 0-7.331-4.282-7.83-5.145a.792.792 0 0 1 0-.792A12.58 12.58 0 0 1 2.378 3.86Zm5.328-2.272c4.726-.143 7.52 4.267 8.028 5.145.15.24.163.542.031.792a12.58 12.58 0 0 1-2.304 2.874l-1.195-1.116a10.846 10.846 0 0 0 1.813-2.154c-.705-1.116-2.937-4.045-6.333-3.958a6.127 6.127 0 0 0-1.346.198L5.149 2.117a7.505 7.505 0 0 1 2.557-.53Zm-.974 5.486v.055c0 .656.532 1.188 1.188 1.188l.047-.008-1.235-1.235Z" fill="#7C8187" />
+                    </svg>
+                  )}
+                </button>
               </div>
-              <ReactMarkdown className="preview" children={markdown} />
+              <div className="preview-inner-container">
+                <div className="preview-width">
+                  <ReactMarkdown className="preview" children={markdown} />
+                </div>
+              </div>
             </div>
           </div>
         </form>
@@ -117,9 +144,9 @@ function App() {
 }
 
 // Modal Component
-function Modal({ showModal, deleteCurrentNote, title }) {
+function Modal({ handleShowModal, deleteCurrentNote, title }) {
   return (
-    <div className="modal" onClick={showModal}>
+    <div className="modal" onClick={handleShowModal}>
       <div role="alertdialog" aria-modal="true" aria-labelledby="dialog_label" aria-describedby="dialog_desc" tabIndex="-1" className="modal__content">
         <h3 id="dialog_label" className="modal__title">
           Delete this document?
@@ -136,7 +163,7 @@ function Modal({ showModal, deleteCurrentNote, title }) {
 }
 
 // Sidebar Component
-function Sidebar({ notes, newNote, updateNote, setNotes, isOpen }) {
+function Sidebar({ notes, newNote, updateNote, isOpen }) {
   return (
     <>
       {isOpen && (
@@ -171,7 +198,7 @@ function Sidebar({ notes, newNote, updateNote, setNotes, isOpen }) {
 }
 
 // Header Component
-function Header({ title, setTitle, deleteCurrentNote, isOpen, handleToggle, showModal }) {
+function Header({ title, setTitle, isOpen, handleToggle, handleShowModal }) {
   return (
     <header>
       <div className="container">
@@ -196,7 +223,7 @@ function Header({ title, setTitle, deleteCurrentNote, isOpen, handleToggle, show
       </div>
 
       <div className="container">
-        <button className="header__delete-btn" onClick={showModal}>
+        <button className="header__delete-btn" onClick={handleShowModal}>
           <svg width="18" height="20" xmlns="http://www.w3.org/2000/svg">
             <path className="trashcan" d="M7 16a1 1 0 0 0 1-1V9a1 1 0 1 0-2 0v6a1 1 0 0 0 1 1ZM17 4h-4V3a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v1H1a1 1 0 1 0 0 2h1v11a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V6h1a1 1 0 0 0 0-2ZM7 3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1H7V3Zm7 14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6h10v11Zm-3-1a1 1 0 0 0 1-1V9a1 1 0 0 0-2 0v6a1 1 0 0 0 1 1Z" fill="#7C8187" />
           </svg>
